8c62e7252febf0086e752cfb8f7f088c328fd179
leodak <arun.kumar19688@gmail.com>
2023-06-25 15:35:03 +1200

    initial commit

---
+++ gallery.js
@@ -0,0 +1,22 @@
+(function ($) {
+
+    var dir = "images/";
+    var fileextension = ".png";
+    $.get({
+        //This will retrieve the contents of the folder if the folder is configured as 'browsable'
+        url: dir,
+        success: function (data) {
+            //List all .png file names in the page
+            $(data).find("a:contains(" + fileextension + ")").each(function () {
+                var filename = this.href.replace(window.location.host, "").replace("http://", "");
+                let imgRowHtml = getImgRow("", dir + filename);
+                $("#imgGallery").append(imgRowHtml);
+            });
+        }
+    });
+
+    function getImgRow(link, imgURL) {
+        var imgRow = `<div class='col-lg-3 col-md-4 col-6'><a href='${link}' class='d-block mb-4 h-100'><img class='img-fluid img-thumbnail' src='${imgURL}' alt=''></a></div>`
+        return imgRow
+    }
+}(jQuery));
