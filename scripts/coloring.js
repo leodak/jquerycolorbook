25883f5dd196499980a8d3f01af188a7c31006c3
leodak <arun.kumar19688@gmail.com>
2023-07-06 18:32:35 +1200

    updated

--- scripts/coloring.js
+++ scripts/coloring.js
@@ -9,7 +9,7 @@
     var svgObject, svgOutline, svgColor
     var swatchUp, swatchDown
     var fillSpeed = 0.15
-    var chosenColor = '#FFFFFF'
+    var chosenColor = '#8E53A1'
     var colors = ['#FFFFFF', '#8E53A1', '#6ABD46', '#71CCDC', '#F7ED45', '#F7DAAF', '#EC2527', '#F16824', '#CECCCC', '#5A499E', '#06753D', '#024259', '#FDD209', '#7D4829', '#931B1E', '#B44426', '#979797', '#C296C5', '#54B948', '#3C75BB', '#F7ED45', '#E89D5E', '#F26F68', '#F37123', '#676868', '#9060A8', '#169E49', '#3CBEB7', '#FFCD37', '#E5B07D', '#EF3C46', '#FDBE17', '#4E4D4E', '#6B449B', '#BACD3F', '#1890CA', '#FCD55A', '#D8C077', '#A62E32', '#F16A2D', '#343433', '#583E98', '#BA539F', '#9D2482', '#DD64A5', '#DB778D', '#EC4394', '#E0398C', '#68AF46', '#4455A4', '#FBEE34', '#AD732A', '#D91E36', '#F99B2A']
     var closeOffset
 
@@ -115,12 +115,14 @@
         mainHolder = this;
         $.get(svgURL, function (data) {
             var $svg = $(data).find('svg');
-            svgObject = $('svg')
-            svgColor = $('g path', $svg);
-            svgOutline = $('g:nth-child(1)', svgObject).children()
-            $(svgColor).on('click', colorMe)
+            // svgObject = $('svg')
+            // svgColor = $('g', $svg);
+            $svg.find('path').on('click', colorMe);
+            $svg.find('path').css('cursor', 'pointer');
+            //svgOutline = $('g:nth-child(1)', svgObject).children()
+            // $(svgColor).on('click', colorMe)
             $("#colorPalette").makeSwatches()
-            $('.swatchHolder').addClass('gray')
+            // $('.swatchHolder').addClass('gray')
             $svg.attr('width', '100%');
             $svg.attr('height', '100%');
             $("#imgToColor").append($svg);
